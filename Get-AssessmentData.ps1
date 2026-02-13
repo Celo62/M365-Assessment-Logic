6ae516fcb64885e8480e20a880158f4c69076621
Celo62 <celil.kuecuek19@gmail.com>
2026-02-13 13:20:42 +0100

    Update Get-AssessmentData.ps1

--- Get-AssessmentData.ps1
+++ Get-AssessmentData.ps1
@@ -1,29 +1,29 @@
-# INHALT FÜR DEINE GITHUB-DATEI: Get-AssessmentData.ps1
 function Get-AssessmentData {
     $results = @{}
-
-    # --- Azure AD / Entra ID ---
-    Write-Host "Extracting Azure AD..." -ForegroundColor Gray
-    $results.AzureAD = @{
-        Organization = Get-MgOrganization
-        Domains      = Get-MgDomain
-        Security     = Get-MgDirectorySetting
+    # Die Basis-URL des Publishers (Thomas Kur)
+    $pubBase = "https://raw.githubusercontent.com/ThomasKur/M365Documentation/master/M365Documentation/Functions/Public"
+    
+    $mapping = @{
+        "AzureAD"  = "$pubBase/AzureAD/Get-M365RepoAzureAD.ps1"
+        "Exchange" = "$pubBase/ExchangeOnline/Get-M365RepoExchangeOnline.ps1"
+        "Intune"   = "$pubBase/Intune/Get-M365RepoIntune.ps1"
+        "Teams"    = "$pubBase/Teams/Get-M365RepoTeams.ps1"
     }
 
-    # --- Exchange Online ---
-    Write-Host "Extracting Exchange..." -ForegroundColor Gray
-    $results.Exchange = @{
-        OrgConfig      = Get-OrganizationConfig
-        TransportConfig = Get-TransportConfig
-        MailFlow       = Get-AcceptedDomain
-    }
+    foreach ($service in $mapping.Keys) {
+        try {
+            Write-Host "Lade Deep-Logic für $service von Publisher..." -ForegroundColor Gray
+            $code = Invoke-RestMethod -Uri $mapping[$service]
+            $sb = [scriptblock]::Create($code)
+            . $sb # Dot-Sourcing der Publisher-Logik
 
-    # --- Intune ---
-    Write-Host "Extracting Intune..." -ForegroundColor Gray
-    $results.Intune = @{
-        DeviceSettings = Get-MgDeviceManagementManagedDevice
-        Policies       = Get-MgDeviceManagementDeviceConfiguration
+            # Dynamischer Aufruf der Publisher-Funktion
+            $funcName = "Get-M365Repo" + ($service -eq "Exchange" ? "ExchangeOnline" : $service)
+            Write-Host "Extrahiere alle Daten für $service..." -ForegroundColor Cyan
+            $results[$service] = Invoke-Expression $funcName
+        } catch {
+            Write-Warning "Konnte $service nicht extrahieren: $($_.Exception.Message)"
+        }
     }
-
     return $results
 }
